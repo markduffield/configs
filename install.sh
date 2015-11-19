3d23269f99fd390767d20c9cc30df96a0380ecf1
Mark Hesketh <contact@markhesketh.co.uk>
2015-11-19 22:08:01 +0000

    Add PHP, Composer and nodejs/NPM to installer.

--- install.sh
+++ install.sh
@@ -2,8 +2,18 @@
 
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-DOTFILES=("aliases" "gitconfig" "gitignore_global" "bashrc" "bash_profile" "bash_aliases" "bash_prompt" "vimrc" "myxkbmap" "xsessionrc" "inputrc")
-
+DOTFILES=(
+    "bashrc"
+    "bash_profile"
+    "bash_aliases"
+    "bash_prompt"
+    "gitconfig"
+    "gitignore_global"
+    "inputrc"
+    "myxkbmap"
+    "xsessionrc"
+    "vimrc"
+)
 
 # Symlinks
 # ==============================================================================
@@ -13,7 +23,7 @@ do
    echo "- ~/.$i"
 done
 
-read -p "We will remove these files if they already exist. Continue? (y/n)" CONT
+read -p "Create these files? The will be overwritten if they exist [y/N]: " CONT
 if [ "$CONT" == "y" ]; then
 
    for i in "${DOTFILES[@]}"
@@ -22,7 +32,37 @@ if [ "$CONT" == "y" ]; then
       rm -f ~/.$i
       ln -nfs ${BASEDIR}/dotfiles/$i ~/.$i
    done
-
-   # Reload shell
-   exec $SHELL -l
 fi
+echo ""
+
+# PHP
+# ==============================================================================
+read -p "Install PHP CLI and Composer? [y/N]: " CONT
+if [ "$CONT" == "y" ]; then
+    echo "Installing PHP CLI (sudo required)"
+    if [ -n "$(which apt-get)" ]; then
+        sudo apt-get install -y php5-cli
+    fi
+
+    echo "Installing Composer"
+    curl -sS https://getcomposer.org/installer | php
+    chmod +x ./composer.phar
+    mkdir -p ~/bin
+    mv ./composer.phar ~/bin/composer
+fi
+echo ""
+
+# NodeJS / NPM
+# ==============================================================================
+read -p "Install NodeJS/NPM? [y/N]: " CONT
+if [ "$CONT" == "y" ]; then
+    echo "Adding nodejs 5.x PPA (sudo required)"
+    if [ -n "$(which apt-get)" ]; then
+        # https://nodejs.org/en/download/package-manager/#debian-and-ubuntu-based-linux-distributions
+        curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
+        sudo apt-get install -y nodejs
+    fi
+fi
+
+# Reload shell
+exec $SHELL -l
