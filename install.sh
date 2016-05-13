780bd798e4a2967fc31f9780ae3d0d60937e8d19
Mark Hesketh <contact@markhesketh.co.uk>
2016-05-13 19:10:03 +0100

    Update installer to only handle dotfiles

--- install.sh
+++ install.sh
@@ -2,12 +2,6 @@
 
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-COMMONPACKAGES=(
-    "curl"
-    "unzip"
-    "build-essential"
-)
-
 DOTFILES=(
     "bashrc"
     "bash_profile"
@@ -20,33 +14,6 @@ DOTFILES=(
     "xsessionrc"
     "vimrc"
 )
-
-PHPSTORMFOLDERS=(
-    "codestyles"
-    "colors"
-    "keymaps"
-)
-
-# Common packages
-# ==============================================================================
-echo "Common packages:"
-for i in "${COMMONPACKAGES[@]}"
-do
-   echo "- $i"
-done
-read -p "Would you like to install these packages? [y/N]: " CONT
-if [ "$CONT" == "y" ]; then
-    if [ -n "$(which apt-get)" ]; then
-        sudo apt-get update
-
-        for i in "${COMMONPACKAGES[@]}"
-        do
-          echo "Installing .$i ..."
-          sudo apt-get install $i
-        done
-    fi
-fi
-echo ""
 
 # Symlinks
 # ==============================================================================
@@ -67,78 +34,6 @@ if [ "$CONT" == "y" ]; then
    done
 fi
 echo ""
-
-# PhpStorm
-# ==============================================================================
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    PHPSTORMDIR="~/Library/Preferences/Webide100"
-else
-    PHPSTORMDIR="~/.WebIde100/config"
-fi
-
-echo "This script will remove and symlink the following PhpStorm folders:"
-for i in "${PHPSTORMFOLDERS[@]}"
-do
-   echo "- ${PHPSTORMDIR}/$i"
-done
-
-read -p "Perform the symlink? Local configs will be overwritten if they exist [y/N]: " CONT
-if [ "$CONT" == "y" ]; then
-    mkdir -p $PHPSTORMDIR
-
-    for i in "${PHPSTORMFOLDERS[@]}"
-    do
-        echo "Symlinking ${PHPSTORMDIR}/$i ..."
-        rm -rf $PHPSTORMDIR/$i
-        ln -nfs $BASEDIR/phpstorm/config/$i $PHPSTORMDIR/$i
-    done
-fi
-echo ""
-
-
-# PHP
-# ==============================================================================
-read -p "Install PHP CLI and Composer? [y/N]: " CONT
-if [ "$CONT" == "y" ]; then
-    echo "Installing PHP CLI (sudo required)"
-    if [ -n "$(which apt-get)" ]; then
-        sudo apt-get install -y php5-cli
-    fi
-
-    echo "Installing Composer"
-    curl -sS https://getcomposer.org/installer | php
-    chmod +x ./composer.phar
-    mkdir -p ~/bin
-    mv ./composer.phar ~/bin/composer
-fi
-echo ""
-
-# NodeJS / NPM
-# ==============================================================================
-read -p "Install NodeJS/NPM? [y/N]: " CONT
-if [ "$CONT" == "y" ]; then
-    echo "Adding nodejs 5.x PPA (sudo required)"
-    if [ -n "$(which apt-get)" ]; then
-        # https://nodejs.org/en/download/package-manager/#debian-and-ubuntu-based-linux-distributions
-        curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
-        sudo apt-get install -y nodejs
-    fi
-fi
-
-
-# Google App Engine
-# ==============================================================================
-read -p "Install Google App Engine (PHP)? [y/N]: " CONT
-if [ "$CONT" == "y" ]; then
-    echo "Downloading and extracting App Engine"
-    wget https://storage.googleapis.com/appengine-sdks/featured/google_appengine_1.9.25.zip -O /tmp/google_appengine.zip
-    sudo unzip /tmp/google_appengine.zip -d /opt
-
-    echo "Installing PHP CGI"
-    if [ -n "$(which apt-get)" ]; then
-        sudo apt-get install php5-cgi
-    fi
-fi
 
 # Reload shell
 exec $SHELL -l
